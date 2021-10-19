8dfb35d131281dd1e9ed423907b3e6ad2ec29e24
Александра Щупоченко <52908598+crngmwng@users.noreply.github.com>
2021-10-19 17:55:21 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function updatePrice() {
-    // Находим select по имени в DOM.
     let s = document.getElementsByName("prodType");
     let select = s[0];
     let n = document.getElementsByName("number");
@@ -11,11 +10,9 @@ function updatePrice() {
         price = num.value*prices.prodTypes[priceIndex];
     }
 
-    // Скрываем или показываем радиокнопки.
     let radioDiv = document.getElementById("radios");
     radioDiv.style.display = (select.value == "2" ? "block" : "none");
 
-    // Смотрим какая товарная опция выбрана.
     let radios = document.getElementsByName("prodOptions");
     radios.forEach(function (radio) {
         if (radio.checked) {
@@ -26,11 +23,9 @@ function updatePrice() {
         }
     });
 
-    // Скрываем или показываем чекбоксы.
     let checkDiv = document.getElementById("checkboxes");
     checkDiv.style.display = (select.value == "3" ? "block" : "none");
 
-    // Смотрим какие товарные свойства выбраны.
     let checkboxes = document.querySelectorAll("#checkboxes input");
     checkboxes.forEach(function (checkbox) {
         if (checkbox.checked) {
@@ -42,7 +37,10 @@ function updatePrice() {
     });
 
     let result = document.getElementById("result");
-    result.innerHTML = price + " рублей";
+
+    if (isNaN(num.value)) {
+        result.innerHTML ="only numbers";
+    } else result.innerHTML = price + " рублей";
 }
 
 function getPrices() {
@@ -60,7 +58,7 @@ function getPrices() {
 }
 
 window.addEventListener('DOMContentLoaded', function (event) {
-    // Скрываем радиокнопки.
+
     let radioDiv = document.getElementById("radios");
     radioDiv.style.display = "none";
 
@@ -70,23 +68,22 @@ window.addEventListener('DOMContentLoaded', function (event) {
     let n = document.getElementsByName("number");
     let num = n[0];
 
-    num.addEventListener("change", function (event) {
+    num.addEventListener("input", function (event) {
         let target = event.target;
         console.log(target.value);
         updatePrice();
     });
 
-    // Находим select по имени в DOM.
     let s = document.getElementsByName("prodType");
     let select = s[0];
-    // Назначаем обработчик на изменение select.
+
     select.addEventListener("change", function (event) {
         let target = event.target;
         console.log(target.value);
         updatePrice();
     });
 
-    // Назначаем обработчик радиокнопок.  
+    
     let radios = document.getElementsByName("prodOptions");
     radios.forEach(function (radio) {
         radio.addEventListener("change", function (event) {
@@ -96,7 +93,7 @@ window.addEventListener('DOMContentLoaded', function (event) {
         });
     });
 
-    // Назначаем обработчик радиокнопок.  
+
     let checkboxes = document.querySelectorAll("#checkboxes input");
     checkboxes.forEach(function (checkbox) {
         checkbox.addEventListener("change", function (event) {
